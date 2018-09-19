210030ebe1ef31e910016171edb0deca79d2cd41
Sulaymon <demirel6777@gmail.com>
2018-09-19 21:06:17 +0300

    add return with new attribute to authcontroller

--- src/main/java/ru/personal/controllers/rest/AuthController.java
+++ src/main/java/ru/personal/controllers/rest/AuthController.java
@@ -1,11 +1,17 @@
 package ru.personal.controllers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.personal.form.UserForm;
 import ru.personal.models.Token;
+import ru.personal.models.User;
 import ru.personal.services.interfaces.AuthenticationService;
+import ru.personal.services.interfaces.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -14,12 +20,21 @@ public class AuthController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Autowired
+    private UserService userService;
+
+    // TODO: 19.09.2018 write it correctly
     @PostMapping(value = "/login")
-    public ResponseEntity<Token> login(@RequestParam(name = "phone") String phone, @RequestParam("pincode")String pincode){
+    public ResponseEntity<?> login(@RequestParam(name = "phone") String phone, @RequestParam("pincode")String pincode){
         UserForm userForm = new UserForm();
         userForm.setPhone(phone);
         userForm.setPincode(pincode);
-        return ResponseEntity.ok(authenticationService.login(userForm));
+        Map<String, Object> map = new HashMap<>();
+        Token token = authenticationService.login(userForm);
+        User user = userService.getUserByToken(token.getToken());
+        map.put("token", token.getToken());
+        map.put("username", user.getUsername());
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 
